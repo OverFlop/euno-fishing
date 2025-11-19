fec68645fc6f66f469288f5dc2e95860d1d9baef
Yaser Salaad <znac332@live.rhul.ac.uk>
2025-11-19 22:50:33 +0000

    #1 Adding gitignore, pom.xml and java files for fish,rarity and difficulty pattern

---
+++ src/main/java/Fish.java
@@ -0,0 +1,73 @@
+package src.main.java.entity;
+
+/**
+ * {@summary}
+ */
+public class Fish {
+  private String name;
+  private DifficultyPattern difficultyPattern;
+  private int difficulty;
+
+  private int exp;
+  private Rarity rarity;
+
+  /**
+   * {@summary}
+   * 
+   * @param name
+   * @param pattern
+   * @param difficulty
+   */
+  public Fish(String name, DifficultyPattern difficultyPattern, int difficulty) {
+    this.name = name;
+    this.difficultyPattern = difficultyPattern;
+    this.difficulty = difficulty;
+
+    if (this.difficulty <= 2) {
+      this.rarity = Rarity.COMMON;
+      this.exp = 5;
+
+    } else if (this.difficulty <= 5 && this.difficulty > 2) {
+      this.rarity = Rarity.UNCOMMON;
+      this.exp = 10;
+
+    } else if (this.difficulty <= 10 && this.difficulty > 5) {
+      this.rarity = Rarity.RARE;
+      this.exp = 20;
+
+    } else if (this.difficulty <= 10 && this.difficulty > 6) {
+      this.rarity = Rarity.UNIQUE;
+      this.exp = 30;
+
+    } else if (this.difficulty <= 30 && this.difficulty > 8) {
+      this.rarity = Rarity.LEGENDARY;
+      this.exp = 60;
+
+    } else if (this.difficulty > 30) {
+      this.rarity = Rarity.UNREAL;
+      this.exp = 100;
+    }
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public DifficultyPattern getDifficultyPattern() {
+    return this.difficultyPattern;
+  }
+
+  public int getDifficulty() {
+    return this.difficulty;
+  }
+
+  public int getExp() {
+    return this.exp;
+  }
+
+  public Rarity getRarity() {
+    return this.rarity;
+  }
+}
+
+
---
+++ src/main/java/Rarity.java
@@ -0,0 +1,8 @@
+package src.main.java.concepts;
+
+/**
+ * {@summary}
+ */
+public enum Rarity {
+  COMMON, UNCOMMON, RARE, UNIQUE, LEGENDARY, UNREAL
+}
